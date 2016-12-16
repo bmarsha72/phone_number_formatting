fd31b6106c9c8813fb8e08862089e2e464192136
Brian Marshall <bmarshall@protonmail.com>
2016-12-16 13:37:51 -0600

    revise

--- phone.js
+++ phone.js
@@ -8,12 +8,11 @@ function validatePhone(phoneno){
     var desired2 = desired.replace(/\s/g,'');
 
     //if there is a leading one, get rid of it:
-    while(desired2.charAt(0) === '1')
-    desired2 = desired2.substr(1);
-
+    while(desired2.charAt(0) === '1'){
+      desired2 = desired2.substr(1);
+    }
     //some more regex formatting:
-  if(desired2.match(/^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i))
-    {
+  if(desired2.match(/^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i)) {
         //confirm that we're at 10 digits:
         if(desired2.length > 10)
         {
@@ -27,9 +26,6 @@ function validatePhone(phoneno){
         {
           console.log('you are good to go, captain');
         }
-    }
-  else
-  {
-    return false;
-  }
-};
+      }
+    else return false;
+  };
